a0447161c5f42f90ff184c71b1467abc0043a9e7
vidyalatha-badde <127079694+vidyalatha-badde@users.noreply.github.com>
2026-06-13 10:04:38 +0530

    Fix import statement for RecursiveCharacterTextSplitter

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import os
 import tempfile
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import FAISS
 from langchain.prompts import PromptTemplate
